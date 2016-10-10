c4d708a1adf1e134973bc7390a1bbd708d8d40f4
Vasil Yordanov <vasil.yordanov@gmail.com>
2016-10-10 18:01:35 +0300

    Implemented Navigation Menu

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.png';
 import './App.css';
 import ConnectionDetailsBox from './ConnectionDetailsBox';
 import ConnectionDetailsClient from './ConnectionDetailsClient';
 import ResourceManagerBox from './ResourceManagerBox';
 import ResourceManagerClient from './ResourceManagerClient';
-import {Toolbar, ToolbarGroup} from 'material-ui/Toolbar';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
-import Popover from 'material-ui/Popover';
-import RaisedButton from 'material-ui/RaisedButton';
-import Menu from 'material-ui/Menu';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui/svg-icons/navigation/menu'
+import ArrowBackIcon from 'material-ui/svg-icons/navigation/arrow-back'
+import Drawer from 'material-ui/Drawer';
+import AppBar from 'material-ui/AppBar';
 import MenuItem from 'material-ui/MenuItem';
 
 // Needed for onTouchTap
@@ -21,69 +22,56 @@ injectTapEventPlugin();
 const lightMuiTheme = getMuiTheme(lightBaseTheme);
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      open: false,
-      value: 3,
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false
+        };
+    }
+
+    handleToggle = () => this.setState({open: !this.state.open});
+
+    handleRequestClose = () => {
+        this.setState({
+            open: false,
+            value: this.state.value
+        });
     };
-  }
 
-  handleTouchTap = (event) => {
-    // This prevents ghost click.
-    event.preventDefault();
+    handleClose = () => this.setState({open: false});
 
-    this.setState({
-      open: true,
-      value: this.state.value,
-      anchorEl: event.currentTarget,
+    handleConnDetailsSelected = () => this.setState({
+        open: false,
+        component: (<ConnectionDetailsBox client={ConnectionDetailsClient} pollInterval={2000}/>)
     });
-  };
 
-  handleRequestClose = () => {
-    this.setState({
-      open: false,
-      value: this.state.value
+    handleResourcesSelected = () => this.setState({
+        open: false,
+        component: (<ResourceManagerBox client={ResourceManagerClient} pollInterval={2000}/>)
     });
-  };
 
-  handleChange = (event, index, value) => this.setState({value});
-  render() {
-    return (
-      <MuiThemeProvider muiTheme={lightMuiTheme}>
-      <div className="App">
-       <Toolbar>
-        <ToolbarGroup firstChild={true}>
-        <RaisedButton
-          onTouchTap={this.handleTouchTap}
-          label="Click me"
-        />
-        <Popover
-          open={this.state.open}
-          anchorEl={this.state.anchorEl}
-          anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
-          targetOrigin={{horizontal: 'left', vertical: 'top'}}
-          onRequestClose={this.handleRequestClose}
-        >
-	<Menu>
-            <MenuItem value={1} primaryText="Connections" />
-            <MenuItem value={2} primaryText="Resources" />
-            <MenuItem value={3} primaryText="Discovery" />
-        </Menu>
-        </Popover>
-        </ToolbarGroup>
-      </Toolbar>
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>NetTransformer</h2>
-        </div>
-       <ConnectionDetailsBox client={ConnectionDetailsClient} pollInterval={2000}  />
-       <br/>
-       <ResourceManagerBox client={ResourceManagerClient} pollInterval={2000}  />
-      </div>
-      </MuiThemeProvider>
-    );
-  }
+    render() {
+        return (
+            <MuiThemeProvider muiTheme={lightMuiTheme}>
+                <div className="App">
+                    <AppBar
+                        title={<span><img src={logo} className="App-logo" alt="logo" />NetTransformer</span>}
+                        onTouchTap={this.handleToggle}
+                        iconElementLeft={<IconButton><MenuIcon /></IconButton>}
+                    />
+                    <Drawer
+                        docked={false}
+                        open={this.state.open}
+                    >
+                        <MenuItem onTouchTap={this.handleClose}><IconButton><ArrowBackIcon /></IconButton></MenuItem>
+                        <MenuItem onTouchTap={this.handleConnDetailsSelected}>Connections</MenuItem>
+                        <MenuItem onTouchTap={this.handleResourcesSelected}>Resources</MenuItem>
+                    </Drawer>
+                    { this.state.component }
+                </div>
+            </MuiThemeProvider>
+        );
+    }
 }
 
 export default App;
